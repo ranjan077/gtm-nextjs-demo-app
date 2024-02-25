c19d0b11ef80ca6240204587317be94ec818bafb
rnagaraju <rnagaraju@carparts.com>
2024-02-25 19:16:02 +0530

    Added button

---
+++ src/app/components/clickMe.jsx
@@ -0,0 +1,6 @@
+
+const ClickMeBtn = () => {
+    return <button id="changebg">Click me to change bg color</button>
+}
+
+export default ClickMeBtn
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import ClickMeBtn from './components/clickMe'
 
 export default function Home() {
   return (
-   <h1>Main page</h1>
+    <>
+      <h1>Main page</h1>
+      <ClickMeBtn />
+    </>
   )
 }
