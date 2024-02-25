b6fc7070a625eb70783e9ca5ef9fc58d70945c63
rnagaraju <rnagaraju@carparts.com>
2024-02-25 19:37:15 +0530

    Added add to cart btn

---
+++ src/app/components/AddToCart.jsx
@@ -0,0 +1,6 @@
+
+const AddToCaRT = () => {
+    return <button id="add-to-cart">Add to cart</button>
+}
+
+export default AddToCaRT
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import ClickMeBtn from './components/clickMe'
+import AddToCart from './components/AddToCart'
 
 export default function Home() {
   return (
     <>
       <h1>Main page</h1>
       <ClickMeBtn />
+      <AddToCart />
     </>
   )
 }
