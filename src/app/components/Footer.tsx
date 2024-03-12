8605635bc8da8b54ec03b7ea82e14f7ceaa57259
rnagaraju <rnagaraju@carparts.com>
2024-03-12 05:53:26 +0530

    styleing cookie settings link

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,13 +1,15 @@
 
 'use client'
 import { useEffect, useState } from 'react'
+import styles from './footer.module.css'
+
 const Footer = () => {
     const [isClient, setIsClient] = useState(false)
     useEffect(() => {
         setIsClient(true)
     }, [])
     return <div>
-         { isClient && <button id="ot-sdk-btn" className="ot-sdk-show-settings">My Cookie Settings</button> }
+         { isClient && <a href="#cookies settings" className={`ot-sdk-show-settings ${styles.cookiesettings}`}>Your Privacy Choices</a> }
     </div>
 }
 
