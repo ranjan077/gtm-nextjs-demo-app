cf36b459131701a3b3348aaa3fc3ffcbfc0b1b36
rnagaraju <rnagaraju@carparts.com>
2024-03-12 06:38:30 +0530

    styleing cookie settings link

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -9,7 +9,9 @@ const Footer = () => {
         setIsClient(true)
     }, [])
     return <div>
-         { isClient && <a href="#cookies settings" className={`ot-sdk-show-settings ${styles.cookiesettings}`}>Your Privacy Choices</a> }
+         { isClient && 
+         <a href="#cookies settings" className={`ot-sdk-show-settings ${styles.cookiesettings}`}>Your Privacy Choices</a> 
+        }
     </div>
 }
 
